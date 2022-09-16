ba28994a502e89a94785ed61f6bb148568f63492
wahid4397 <101095300+wahid4397@users.noreply.github.com>
2022-09-15 20:17:56 -0700

    Add files via upload

---
+++ Largestnumber3.java
@@ -0,0 +1,31 @@
+/* To find largest of 03 number*/
+
+
+class Largestnumber3
+{
+	public static void main(String[] args) {
+		
+		int a=50, b=100, c=500;
+		if(a>b)
+		{
+			if(a>c)
+				System.out.print(a);
+			else
+				System.out.print(c);
+		}
+		else
+		{
+			
+		
+			if(b>c)
+				System.out.print(b);
+			else
+				System.out.print(c);
+	
+			
+		}
+	}				
+		
+	}
+
+
---
+++ Patternprint.java
@@ -0,0 +1,26 @@
+/* Write a program to print given pattern*/
+
+class Patternprint
+{
+	public static void main(String[] args)
+	{
+		int a,b,c;
+		for(a=5;a<=5;a++)
+			
+		{
+			for(b=4;b<a;b++)
+			
+				
+			{
+			System.out.println("* * * * * ");
+			System.out.println("* * * * ");
+		}
+		for(c=5;c<=a;c++)
+		{
+			System.out.println("* * *  ");
+		}
+		System.out.println("*");
+				
+		}
+	}
+}
